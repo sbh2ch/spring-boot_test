0b86b8ccf0eef22bad9bd0638b31fdbd78ab2654
kiost-PC\kiost <sbh2ch@gmail.com>
2017-05-23 19:23:36 +0900

    init

---
+++ src/main/java/com/son/Application.java
@@ -0,0 +1,14 @@
+package com.son;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by kiost on 2017-05-20.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/son/account/Account.java
@@ -0,0 +1,8 @@
+package com.son.account;
+
+/**
+ * Created by kiost on 2017-05-23.
+ */
+public class Account {
+
+}
